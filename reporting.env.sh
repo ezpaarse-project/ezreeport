dc7f80c92cc966876971f18ddf5685860ded4aab
oxypomme <oxytom8@gmail.com>
2022-10-18 15:31:55 +0200

    feat: Added base services

---
+++ reporting.env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+LOCAL_ENV_FILE="$SCRIPT_DIR/reporting.local.env.sh"
+
+export REDIS_PASSWORD=""
+# export ANY="value"
+
+if [[ -f $LOCAL_ENV_FILE ]] ; then
+  source "$LOCAL_ENV_FILE"
+fi
