b63dc4f4971f6084982bf488b774006dba3c59e1
oxypomme <oxytom8@gmail.com>
2022-10-20 10:24:52 +0200

    feat(report): Added missing implementation of custom codes for success
    + refactored ` res.errorJson`

--- src/report/middlewares/format.ts
+++ src/report/middlewares/format.ts
@@ -1,31 +1,27 @@
 import type { RequestHandler } from 'express';
-import { getReasonPhrase, ReasonPhrases, StatusCodes } from 'http-status-codes';
+import { getReasonPhrase, StatusCodes } from 'http-status-codes';
 import { CustomError } from '../types/errors';
 
 /**
  * API formatter middleware
  */
 const middleware: RequestHandler = (_req, res, next) => {
-  res.sendJson = (content: any) => {
-    res.status(StatusCodes.OK).json({
+  res.sendJson = (content: any, code = StatusCodes.OK) => {
+    res.status(code).json({
       status: {
-        code: StatusCodes.OK,
-        message: ReasonPhrases.OK,
+        status: {
+          code,
+          message: getReasonPhrase(code),
+        },
+        content,
       },
-      content,
     });
   };
   res.errorJson = (error: CustomError | Error) => {
     const err = error instanceof CustomError ? error : new CustomError(error.message, 500);
-    res.status(err.code).json({
-      status: {
-        code: err.code,
-        message: getReasonPhrase(err.code),
-      },
-      content: {
-        message: error.message,
-      },
-    });
+    res.sendJson({
+      message: error.message,
+    }, err.code);
   };
 
   next();
