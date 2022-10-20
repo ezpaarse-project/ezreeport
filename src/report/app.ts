f7de98b6b0a204908530e0222fe9785def5a8c75
oxypomme <oxytom8@gmail.com>
2022-10-20 10:55:48 +0200

    docs(report): Added openapi spec
    + added swagger routes

--- src/report/app.ts
+++ src/report/app.ts
@@ -1,12 +1,17 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 import logger from './lib/logger';
 import formatMiddleware from './middlewares/format';
 import loggerMiddleware from './middlewares/logger';
+import openapi from './openapi.json';
 
 const app = express();
 const port = 8080;
 
 app.use(loggerMiddleware, formatMiddleware);
+
+app.use('/openapi.json', (_req, res) => res.json(openapi));
+app.use('/', swaggerUi.serve, swaggerUi.setup(openapi));
 
 /**
  * 404 Fallback
