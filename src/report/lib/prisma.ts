02d6886779f8361342cd2b0f328781605de4a42d
oxypomme <oxytom8@gmail.com>
2022-10-21 14:33:01 +0200

    feat(report): Added logs to prisma

--- src/report/lib/prisma.ts
+++ src/report/lib/prisma.ts
@@ -1,3 +1,19 @@
 import { PrismaClient } from '@prisma/client';
+import logger from './logger';
 
-export default new PrismaClient();
+const client = new PrismaClient({
+  log: [
+    { level: 'query', emit: 'event' },
+    { level: 'info', emit: 'event' },
+    { level: 'warn', emit: 'event' },
+    { level: 'error', emit: 'event' },
+  ],
+  errorFormat: 'pretty',
+});
+
+client.$on('query', (e) => logger.debug(`[prisma] ${e.query} - ${e.params} (${e.duration}ms)`));
+client.$on('info', (e) => logger.info(`[prisma] ${e.message}`));
+client.$on('warn', (e) => logger.warn(`[prisma] ${e.message}`));
+client.$on('error', (e) => logger.error(`[prisma] ${e.message}`));
+
+export default client;
