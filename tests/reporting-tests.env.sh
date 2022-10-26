5ad025e67b05bdb4e1bd38aeb3d1163c8efbe405
oxypomme <oxytom8@gmail.com>
2022-10-26 09:53:51 +0200

    feat(tests): Switcched axios to chai
    + added config

---
+++ tests/reporting-tests.env.sh
@@ -0,0 +1,7 @@
+export EZMESURE_TOKEN=""
+export REPORT_API=""
+
+if [[ -f $LOCAL_ENV_FILE ]] ; then
+  source "$LOCAL_ENV_FILE"
+fi
+
--- tests/src/report.spec.ts
+++ tests/src/report.spec.ts
@@ -1,26 +1,40 @@
-import assert from 'assert';
-import axios from 'axios';
-import { StatusCodes } from 'http-status-codes';
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import { randomBytes } from 'crypto';
+import config from '../lib/config';
 
-const http = axios.create({
-  baseURL: 'http://localhost:8080',
-  validateStatus: () => true,
-});
+const { expect } = chai;
+
+chai.use(chaiHttp);
+
+const request = chai.request(config.get('reportUrl'));
 
 describe('Report', () => {
   describe('General', () => {
-    it('ping should return 204', async () => {
-      const { status } = await http.get<void>('/ping');
-      assert.equal(status, StatusCodes.NO_CONTENT);
+    it('ping should return 204', (done) => {
+      request.get('/ping').end((_err, res) => {
+        expect(res).to.have.status(204);
+        done();
+      });
     });
 
-    it('random should return 404', async () => {
-      const { status } = await http.get<void>('/ghfigyzqdsfuoqdtqsddfqs');
-      assert.equal(status, StatusCodes.NOT_FOUND);
+    it('random should return 404', (done) => {
+      const url = `/${randomBytes(20).toString('base64')}`;
+      request.get(url).end((_err, res) => {
+        expect(res).to.have.status(404);
+        done();
+      });
     });
   });
 
   describe('Tasks', () => {
+    const task: any = {
+      layout: { noLayout: true },
+      targets: ['tom.sublet@inist.fr'],
+      recurrence: 'WEEKLY',
+      nextRun: new Date(2022, 11, 25),
+      enabled: false,
+    };
     // it('should return ', () => {
     //   assert.equal([1, 2, 3].indexOf(4), -1);
     // });
