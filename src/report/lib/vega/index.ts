797fca2d96591739bd00960c24b9f5fbe575bfe6
oxypomme <oxytom8@gmail.com>
2022-11-03 08:47:10 +0100

    feat(report): Rewrited `createVegaLSpec` to make it simpler
    All params have been revamped. "test" layout doesn't exist anymore.

--- src/report/lib/vega/index.ts
+++ src/report/lib/vega/index.ts
@@ -1,86 +1,87 @@
 import { registerFont } from 'canvas';
 import { writeFile } from 'fs';
 import type { ImageOptions } from 'jspdf';
-import { merge, omit, pick } from 'lodash';
+import { cloneDeep, merge, omit } from 'lodash';
 import { join } from 'path';
 import {
   expressionFunction,
   Locale as VegaLocale,
   parse,
+  scheme,
   View
 } from 'vega';
 import { compile, type TopLevelSpec } from 'vega-lite';
-import type { Mark } from 'vega-lite/build/src/mark';
-import type { LayerSpec, UnitSpec } from 'vega-lite/build/src/spec';
+import type { Mark, MarkDef } from 'vega-lite/build/src/mark';
+import type { UnitSpec } from 'vega-lite/build/src/spec';
 import config from '../config';
 import logger from '../logger';
 import type { PDFReport, PDFReportOptions } from '../pdf';
 import type { TableParams, TableParamsFnc } from '../pdf/table';
 import localeFR from './locales/fr-FR.json';
+import VegaLogger from './logger';
 
 const rootPath = config.get('rootPath');
 const { outDir } = config.get('pdf');
+
 registerFont('lib/vega/fonts/Roboto-light.ttf', { family: 'Roboto', weight: 'normal' });
 registerFont('lib/vega/fonts/Roboto-medium.ttf', { family: 'Roboto', weight: 'bold' });
-
-type LayerType = LayerSpec<any> | UnitSpec<any>;
-
-type FormatFnc = (v: number | string) => string;
+// Default colors of labels for default colors of Vega (https://vega.github.io/vega/docs/schemes/)
+scheme('tableau10-labels', [
+  'white', // white on blue
+  'black', // black on orange
+  'black', // black on red
+  'black', // black on cyan
+  'black', // black on green
+  'black', // black on yellow
+  'black', // black on purple
+  'black', // black on pink
+  'white', // white on brown
+  'black', // black on grey
+]);
 
 // TODO[type]: Rework types (no more any)
 
 /**
  * Params for createVegaLSpec
  */
-interface VegaParams {
-  /**
-   * Title of the graph
-   */
-  title: Exclude<TopLevelSpec['title'], undefined>;
-  /**
-   * Vega-lite spec with some limitations
-   */
-  spec: Omit<TopLevelSpec, 'type' | 'datasets' | 'data'> & { encoding: LayerType['encoding'] };
-  /**
-   * Width of the graph, should match PDF viewport
-   */
-  width?: number;
-  /**
-   * Width of the graph, should match PDF viewport
-   */
-  height?: number;
-  /**
-   * Should show datalabels or not
-   */
-  dataLabel?:
-  | true
-  | {
-    /**
-     * Spec of the layer
-     */
-    spec: Omit<LayerType, 'mark'> & { mark?: any };
-    /**
-     * Custom formatter
-     */
-    format?: FormatFnc | 'percent' | { type: 'percent', minValue?: number };
-  };
-  /**
-   * Spec of the data layer
-   */
-  dataLayer?: Omit<LayerType, 'mark'>;
-  /**
-   * Should export generated spec (without data) to debug file ?
-   */
-  debugExport?: boolean;
-}
+type Layer = UnitSpec<string>;
+type Title = Exclude<Layer['title'], undefined>;
+type Encoding = Exclude<Layer['encoding'], undefined>;
+// Hide 'mark.type' property
+type CustomLayer = Omit<Layer, 'mark'> & { mark: Omit<Layer['mark'], 'type'> };
 
+type VegaParams = {
+  /**
+   * Width of the graph, should match PDF viewport
+   */
+  width: number;
+  /**
+   * Width of the graph, should match PDF viewport
+   */
+  height: number;
+  debugExport?: boolean,
+  dataLayer?: CustomLayer;
+  value: Exclude<Encoding['x' | 'y' | 'theta'], undefined | null> & { field: string };
+  label: Exclude<Encoding['x' | 'y' | 'color'], undefined | null> & { field: string },
+  color?: Encoding['color'] & { field: string },
+  title: Title,
+  dataLabel?: {
+    format: 'percent' | 'numeric',
+    showLabel?: boolean
+    minValue?: number,
+  }
+};
+
+export type InputVegaParams = Omit<VegaParams, 'width' | 'height'>;
+
+type AnyTableParam = TableParams | TableParamsFnc | Promisify<TableParamsFnc>;
 /**
  * Figure definition
  */
 export interface VegaFigure<Type extends Mark | 'table'> {
   type: Type;
   data: any;
-  params: Type extends Mark ? VegaParams : TableParams | TableParamsFnc | Promisify<TableParamsFnc>;
+  params: Type extends Mark ? InputVegaParams : AnyTableParam;
 }
 
 /**
@@ -98,10 +99,6 @@ export type LayoutVegaFigure = Array<AnyVegaFigureFnc | Promisify<AnyVegaFigureF
 /**
  * Helper to create Vega-lite spec
  *
- * Some values are by default :
- * - Datalabels default to Y values
- * - Default locale is fr-FR
- *
  * @param type Type of graph
  * @param data The data
  * @param params Graph options
@@ -112,7 +109,161 @@ export const createVegaLSpec = (
   data: any[],
   params: VegaParams,
 ): TopLevelSpec => {
-  // TODO?[feat]: Merge with whole params.spec
+  const layers: Layer[] = [];
+  // Adding default layer
+  const dataLayer = merge<Layer, CustomLayer | {}>(
+    {
+      mark: {
+        type,
+        point: true,
+        radius2: params.height / 5,
+      },
+    },
+    params.dataLayer ?? {},
+  );
+  layers.push(dataLayer);
+
+  // Getting default encoding
+  let encoding: Encoding = {
+    color: {
+      scale: {
+        // Default colors of Vega (https://vega.github.io/vega/docs/schemes/)
+        scheme: 'tableau10',
+      },
+    },
+  };
+  switch (type) {
+    case 'arc':
+      encoding = merge<Encoding, Encoding>(encoding, {
+        theta: {
+          stack: true,
+          type: 'quantitative',
+          ...params.value,
+        },
+        order: {
+          // @ts-ignore
+          sort: 'descending',
+          // @ts-ignore
+          type: 'quantitative',
+          ...params.value,
+        },
+        color: {
+          // @ts-ignore
+          sort: {
+            // @ts-ignore
+            field: params.value.field,
+            // @ts-ignore
+            order: params.value.order ?? 'descending',
+          },
+          ...params.label,
+        },
+      });
+      break;
+
+    case 'bar':
+      encoding = merge<Encoding, Encoding>(encoding, {
+        y: {
+          stack: 'zero',
+          type: 'quantitative',
+          ...params.value,
+        },
+        x: {
+          type: 'nominal',
+          ...params.label,
+        },
+        color: {
+          ...params.color,
+        },
+      });
+      break;
+
+    default:
+      break;
+  }
+
+  // Adding datalabels
+  if (params.dataLabel) {
+    const dLLayer: Layer = {
+      mark: {
+        type: 'text',
+        align: 'center',
+        baseline: 'top',
+        dy: 5,
+        radius: type === 'arc' ? (params.height ?? 0) / 2.9 : undefined,
+      },
+      encoding: {
+        text: {
+        },
+        // FIXME: "WARN Dropping {"legend":null,"scale":{"scheme":"tableau10-labels"}} from channel "color" since it does not contain any data field, datum, value, or signal."
+        color: {
+          legend: null,
+          scale: {
+            scheme: 'tableau10-labels',
+          },
+        },
+      },
+    };
+
+    // Format datalabels
+    let condition: string | undefined;
+    let format: ((v: string) => string) | undefined;
+    switch (params.dataLabel.format) {
+      case 'percent': {
+        const totalDocs = data.reduce((prev, value) => prev + value[params.value.field], 0);
+        const minValue = params.dataLabel.minValue ?? 0.02;
+        condition = `datum['${params.value.field}'] / ${totalDocs} >= ${minValue}`;
+
+        format = (v) => {
+          const perc = +v / totalDocs;
+          return perc.toLocaleString('fr-FR', { style: 'percent', maximumFractionDigits: 2 });
+        };
+        break;
+      }
+
+      default: {
+        if (params.dataLabel.minValue) {
+          const { minValue } = params.dataLabel;
+          condition = `datum['${params.value.field}'] >= ${minValue}`;
+        }
+        break;
+      }
+    }
+    // Adding condition to datalabels
+    if (dLLayer.encoding?.text) {
+      // @ts-ignore
+      dLLayer.encoding.text.condition = {
+        test: condition ?? 'true',
+        ...params.value,
+      };
+    }
+
+    // Showing label if needed
+    if (params.dataLabel.showLabel) {
+      layers.push({
+        mark: merge(cloneDeep(dLLayer.mark), {
+          dy: -7,
+        }),
+        encoding: {
+          text: {
+            condition: {
+              test: condition ?? 'true',
+              field: params.label.field,
+            },
+          },
+          color: dLLayer.encoding?.color,
+        },
+      });
+      (dLLayer.mark as MarkDef<'text'>).fontWeight = 'bold';
+    }
+
+    if (format) {
+      expressionFunction('dataLabelFormat', format);
+      (dLLayer.encoding?.text?.condition as any).format = '';
+      (dLLayer.encoding?.text?.condition as any).formatType = 'dataLabelFormat';
+    }
+
+    layers.push(dLLayer);
+  }
 
   const spec: TopLevelSpec = {
     $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
@@ -121,146 +272,25 @@ export const createVegaLSpec = (
     background: 'transparent',
     title: merge<TopLevelSpec['title'], TopLevelSpec['title'] | {}>(
       {
-        text: typeof params.title !== 'object' ? params.title : '',
+        text: '',
         anchor: 'start',
         dy: -5,
       },
-      typeof params.title === 'object' ? params.title : {},
+      params.title,
     ),
     // Adding data
     datasets: {
       default: data,
     },
     data: { name: 'default' },
-    // Allow specific color for specific layers
-    resolve: merge<TopLevelSpec['resolve'], TopLevelSpec['resolve'] | {}>(
-      {
-        scale: {
-          color: 'independent',
-        },
-      },
-      typeof params.spec?.resolve === 'object' ? params.spec.resolve : {},
-    ),
-    layer: [
-      // Default layer with the main chart
-      merge<LayerType, LayerType | {}>(
-        { mark: { type, point: true, radius2: (params.height ?? 0) / 5 } },
-        params.dataLayer ?? {},
-      ),
-    ],
-    encoding: merge<LayerType['encoding'], LayerType['encoding']>(
-      {
-        color: {
-          scale: {
-            // Default colors of Vega (usefull for reference or datalabels)
-            range: [
-              '#4c78a8', // blue
-              '#f58518', // orange
-              '#e45756', // red
-              '#72b7b2', // cyan
-              '#54a24b', // green
-              '#eeca3b', // yellow
-              '#b279a2', // purple
-              '#ff9da6', // pink
-              '#9d755d', // brown
-              '#bab0ac', // grey
-            ],
-          },
-        },
-      },
-      params.spec.encoding,
-    ),
+    layer: layers,
+    encoding,
     config: {
-      locale: params.spec.config?.locale ?? (localeFR as VegaLocale),
+      locale: localeFR as VegaLocale,
       customFormatTypes: true,
       font: 'Roboto',
     },
   };
-
-  // FIXME: correctly guess value
-  const yField = spec.encoding?.y ?? spec.layer[0].encoding?.y;
-  // const xField = spec.encoding?.x ?? spec.layer[0].encoding?.x;
-
-  if (params.dataLabel) {
-    const dLLayer = merge<LayerType, LayerType | {}>(
-      // Default config for data labels
-      {
-        mark: {
-          type: 'text',
-          align: 'center',
-          baseline: 'top',
-          dy: 5,
-          radius: type === 'arc' ? (params.height ?? 0) / 2.9 : undefined,
-        },
-        encoding: {
-          text: {
-            field: (yField as any)?.field,
-            type: (yField as any)?.type,
-            aggregate: (yField as any)?.aggregate,
-          },
-          color: {
-            // Get some fields of global color encoding
-            ...(params.spec.encoding?.color != null
-              ? pick(params.spec.encoding.color, 'field', 'type')
-              : {}),
-            // @ts-expect-error
-            legend: null,
-            scale: {
-              // Based on default Vega colors
-              range: [
-                'white', // white on blue
-                'black', // black on orange
-                'black', // black on red
-                'black', // black on cyan
-                'black', // black on green
-                'black', // black on yellow
-                'black', // black on purple
-                'black', // black on pink
-                'white', // white on brown
-                'black', // black on grey
-              ],
-            },
-          },
-        },
-      },
-      typeof params.dataLabel === 'object' ? params.dataLabel.spec : {},
-    );
-
-    // Resolve custom format
-    if (
-      typeof params.dataLabel === 'object'
-      && dLLayer.encoding?.text != null
-    ) {
-      const dlParams = params.dataLabel;
-      const { field } = dLLayer.encoding.text as any;
-
-      // Prefine percent format
-      if (
-        dlParams.format === 'percent'
-        || (typeof dlParams.format !== 'function' && dlParams.format?.type === 'percent')
-      ) {
-        const totalDocs = data.reduce((prev, value) => prev + value[field], 0);
-        const minValue = typeof dlParams.format === 'object' && dlParams.format.minValue
-          ? dlParams.format.minValue
-          : 0.02;
-
-        dlParams.format = (v) => {
-          const perc = +v / totalDocs;
-          return perc >= minValue
-            ? perc.toLocaleString('fr-FR', { style: 'percent', maximumFractionDigits: 2 })
-            : '';
-        };
-      }
-
-      if (typeof dlParams.format === 'function') {
-        expressionFunction('dataLabelFormat', params.dataLabel.format);
-        (dLLayer.encoding.text as any).format = '';
-        (dLLayer.encoding.text as any).formatType = 'dataLabelFormat';
-      }
-    }
-
-    spec.layer.push(dLLayer);
-  }
 
   // Write generated spec into debug file (without data to gain time & space)
   if (params.debugExport === true) {
@@ -285,7 +315,7 @@ export const createVegaLSpec = (
  * @param spec The Vega-lite spec
  * @returns The vega View
  */
-export const createVegaView = (spec: TopLevelSpec): View => new View(parse(compile(spec).spec), { renderer: 'none' });
+export const createVegaView = (spec: TopLevelSpec): View => new View(parse(compile(spec).spec), { renderer: 'none' }).logger(new VegaLogger());
 
 /**
  * Shorthand to add a Vega View to the PDF
