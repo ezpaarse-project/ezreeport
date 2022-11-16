48fd71fd5bbde25fb7c448cf0456cb8668a4bd4c
oxypomme <oxytom8@gmail.com>
2022-11-16 10:08:45 +0100

    feat(mail): Added missing config from report service

--- src/mail/.eslintrc.js
+++ src/mail/.eslintrc.js
@@ -4,14 +4,30 @@ module.exports = {
     node: true,
   },
   extends: ['airbnb-base', 'airbnb-typescript/base'],
-  overrides: [
-  ],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: 'src/mail/tsconfig.json'
+    project: 'src/mail/tsconfig.json',
   },
   rules: {
-    "import/extensions": "off"
+    'import/extensions': 'off',
+    '@typescript-eslint/comma-dangle': ['error', {
+      // Defaulting to airbnb config
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'always-multiline',
+      enums: 'always-multiline',
+      generics: 'always-multiline',
+      tuples: 'always-multiline',
+      // Expect for imports, because it conflict with VSCode
+      imports: 'never',
+    }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', {
+      argsIgnorePattern: '^_',
+      destructuredArrayIgnorePattern: '^_',
+    }],
   },
 };
