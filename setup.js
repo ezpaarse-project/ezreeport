511a08f38938b8895daac5d5e6efb1497518a4b1
oxypomme <oxytom8@gmail.com>
2022-11-17 10:09:11 +0100

    fix(tests): fixed global config

---
+++ setup.js
@@ -0,0 +1,35 @@
+const fs = require('node:fs');
+const path = require('node:path')
+const { execSync } = require('node:child_process')
+
+const BASE_PATH = path.join(__dirname, 'src');
+const services = fs.readdirSync(BASE_PATH);
+
+// Install main
+const mainPackageLock = path.join(__dirname, 'package-lock.json');
+if (fs.existsSync(mainPackageLock)) {
+  console.log('Installing "main" dependencies');
+  execSync('npm ci', { cwd: __dirname });
+}
+
+const localEnv = path.join(__dirname, 'reporting.local.env.sh');
+if (!fs.existsSync(localEnv)) {
+  console.log('Creating "reporting.local.env.sh"');
+  fs.writeFileSync(localEnv, 'export DATABASE_URL="$DATABASE_PROTOCOL://$DATABASE_USER:$DATABASE_PASSWORD@$DATABASE_HOST:$DATABASE_PORT/$DATABASE_DB?schema=default"')
+}
+
+// Install tests
+const testsPackageLock = path.join(__dirname, 'tests/package-lock.json');
+if (fs.existsSync(testsPackageLock)) {
+  console.log('Installing "tests" dependencies');
+  execSync('npm ci', { cwd: path.join(__dirname, 'tests') });
+}
+
+// Install services
+for (const service of services) {
+  const packageLock = path.join(BASE_PATH, service, 'package-lock.json');
+  if (fs.existsSync(packageLock)) {
+    console.log(`Installing "${service}" dependencies`);
+    execSync('npm ci', { cwd: path.join(BASE_PATH, service) });
+  }
+}
