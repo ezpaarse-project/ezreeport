c617975a0bc486f0a23bd9c721125709a01696cc
oxypomme <oxytom8@gmail.com>
2022-11-24 09:27:42 +0100

    refactor(report): Uniformized configs & env default

--- reporting.env.sh
+++ reporting.env.sh
@@ -3,20 +3,20 @@
 SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
 LOCAL_ENV_FILE="$SCRIPT_DIR/reporting.local.env.sh"
 
-export REDIS_HOST=""
-export REDIS_PORT=""
+export REDIS_HOST="redis"
+export REDIS_PORT=6379
 export REDIS_PASSWORD=""
-export REDIS_CONCURRENCE=""
+export REDIS_CONCURRENCE=5
 
 export ELASTIC_SCHEME="https"
-export ELASTIC_HOST=""
-export ELASTIC_PORT="9200"
+export ELASTIC_HOST="host.docker.internal"
+export ELASTIC_PORT=9200
 export ELASTIC_API_KEY="" # Base 64
 export ELASTIC_URL="$ELASTIC_SCHEME://$ELASTIC_HOST:$ELASTIC_PORT"
 
 export DATABASE_PROTOCOL="postgresql"
 export DATABASE_USER="postgres"
-export DATABASE_PASSWORD="changeme"
+export DATABASE_PASSWORD=""
 export DATABASE_HOST="db"
 export DATABASE_PORT=5432
 export DATABASE_DB="postgres"
@@ -24,27 +24,29 @@ export DATABASE_URL="$DATABASE_PROTOCOL://$DATABASE_USER:$DATABASE_PASSWORD@$DAT
 
 export SMTP_HOST="maildev"
 export SMTP_PORT=1025
-export SMTP_SECURE=""
-export SMTP_IGNORE_TLS=""
-export SMTP_REJECT_UNAUTHORIZED=""
+export SMTP_SECURE="false"
+export SMTP_IGNORE_TLS="false"
+export SMTP_REJECT_UNAUTHORIZED="false"
 
-export EMAIL_SENDER=""
-export EMAIL_EZTEAM=""
-export EMAIL_ATTEMPTS=""
-export EMAIL_ATTEMPTS_INTERVAL=""
+export EMAIL_SENDER="ezmesure-reporting@couperin.org"
+export EMAIL_EZTEAM="ezteam@couperin.org"
+export EMAIL_ATTEMPTS=5
+export EMAIL_ATTEMPTS_INTERVAL=200
 
-export EZMESURE_AUTH_SECRET=""
+export EZMESURE_AUTH_SECRET="" # ezMesure API key
+export EZMESURE_DEPOSITORS_INDEX="depositors"
 
-export CRON_GENERATE_REPORT=""
-export CRON_PURGE_OLD_REPORT=""
+export CRON_GENERATE_REPORT="0 0 * * *"
+export CRON_PURGE_OLD_REPORT="0 0 * * *"
 
 export MONITORING_INDEX_PREFIX="reporting-"
 export MONITORING_API_KEY="" # "Beats" version of ELASTIC_API_KEY
 export MONITORING_SCHEDULE="@every 5s"
 
-export LOG_LEVEL=""
+export API_DOMAIN="http://localhost:8080"
+export API_ALLOWED_ORIGINS="*" # Comma separated origins (or * to allow all)
 
-export ALLOWED_ORIGINS="*" # Comma separated origins (or * to allow all)
+export LOG_LEVEL="info"
 
 if [[ -f $LOCAL_ENV_FILE ]] ; then
   source "$LOCAL_ENV_FILE"
