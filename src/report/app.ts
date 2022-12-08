c0e051f82c3256d7889cce69534f8fdc51a0c6a4
oxypomme <oxytom8@gmail.com>
2022-12-08 15:01:33 +0100

    refactor(report): Moved some const about http & elastic to env
    + moved cros to dedicated file

--- src/report/app.ts
+++ src/report/app.ts
@@ -1,4 +1,3 @@
-import cors from 'cors';
 import express from 'express';
 import { StatusCodes } from 'http-status-codes';
 import swaggerUi from 'swagger-ui-express';
@@ -6,6 +5,7 @@ import config from './lib/config';
 import './lib/datefns'; // Setup default options for date-fns
 import './lib/elastic/apm'; // Setup Elastic's APM for monitoring
 import logger from './lib/logger';
+import corsMiddleware from './middlewares/cors';
 import formatMiddleware from './middlewares/format';
 import loggerMiddleware from './middlewares/logger';
 import openapi from './openapi.json';
@@ -19,25 +19,12 @@ import templatesRouter from './routes/templates';
 import unsubscribeRouter from './routes/unsubscribe';
 import { HTTPError } from './types/errors';
 
-const allowedOrigins = (config.get('allowedOrigins')).split(',');
-
 const app = express();
-const port = 8080;
+const port = config.get('port');
 
 app.use(
-  cors({
-    origin: (origin, next) => {
-      if (
-        allowedOrigins[0] === '*'
-        || (origin && allowedOrigins.indexOf(origin) !== -1)
-      ) {
-        next(null, true);
-      } else {
-        next(new Error('Not allowed by CORS'));
-      }
-    },
-  }),
   express.json(),
+  corsMiddleware,
   loggerMiddleware,
   formatMiddleware,
 );
---
+++ src/report/middlewares/cors.ts
@@ -0,0 +1,17 @@
+import cors from 'cors';
+import config from '../lib/config';
+
+const allowedOrigins = (config.get('allowedOrigins')).split(',');
+
+export default cors({
+  origin: (origin, next) => {
+    if (
+      allowedOrigins[0] === '*'
+      || (origin && allowedOrigins.indexOf(origin) !== -1)
+    ) {
+      next(null, true);
+    } else {
+      next(new Error('Not allowed by CORS'));
+    }
+  },
+});
