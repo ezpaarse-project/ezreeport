133b66c0243f50b055562293fc45706bffc1954e
oxypomme <oxytom8@gmail.com>
2022-12-14 10:38:17 +0100

    chore: better precommit lint
    + commintlint

--- tests/lib/utils.ts
+++ tests/lib/utils.ts
@@ -1,5 +1,6 @@
 import { randomBytes } from 'node:crypto';
 
+// eslint-disable-next-line import/prefer-default-export
 export const randomString = () => randomBytes(20)
   .toString('base64')
   .replace(/[?/]/g, '_');
