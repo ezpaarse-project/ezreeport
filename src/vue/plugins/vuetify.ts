04985653ebf926cf321793f0b7233a9b1fe089cc
oxypomme <oxytom8@gmail.com>
2023-01-05 13:44:02 +0100

    feat(vue): added support for mdi

--- src/vue/plugins/vuetify.ts
+++ src/vue/plugins/vuetify.ts
@@ -1,6 +1,13 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+import '@mdi/font/css/materialdesignicons.min.css';
+
+// Vuetify plugin for Histoire
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  icons: {
+    iconfont: 'mdi',
+  },
+});
