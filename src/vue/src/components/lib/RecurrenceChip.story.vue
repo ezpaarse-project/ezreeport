2a13a2efb3125d51ce93ebcc51520fda5c80a97e
oxypomme <oxytom8@gmail.com>
2023-01-13 14:34:04 +0100

    feat(vue): added chip for recurrence

---
+++ src/vue/src/components/lib/RecurrenceChip.story.vue
@@ -0,0 +1,87 @@
+<template>
+  <Story
+    title="internal/RecurrenceChip"
+    group="task"
+  >
+    <template #controls>
+      <HstSelect
+        v-model="current"
+        :options="options"
+        title="Current"
+      />
+      <HstSelect
+        v-model="size"
+        :options="sizes"
+        title="Size"
+      />
+    </template>
+
+    <Variant
+      title="Light theme"
+      icon="material-symbols:light-mode-outline"
+    >
+      <v-app style="background: transparent">
+        <v-theme-provider light>
+          <RecurrenceChip
+            :value="current"
+            :size="size"
+            class="mb-4"
+          />
+        </v-theme-provider>
+        <HstLocale
+          class="mt-4"
+          direction="bottom"
+        />
+      </v-app>
+    </Variant>
+
+    <Variant
+      title="Dark theme"
+      icon="material-symbols:dark-mode-outline"
+    >
+      <v-app style="background: transparent">
+        <v-theme-provider dark>
+          <RecurrenceChip
+            :value="current"
+            :size="size"
+            class="mb-4"
+          />
+        </v-theme-provider>
+        <HstLocale
+          class="mt-4"
+          direction="bottom"
+        />
+      </v-app>
+    </Variant>
+  </Story>
+</template>
+
+<script setup lang="ts">
+import { tasks } from 'ezreeport-sdk-js';
+import { ref } from 'vue';
+import type { Sizes } from './RecurrenceChip.vue';
+
+const current = ref(tasks.Recurrence.DAILY);
+const options = ref([
+  tasks.Recurrence.DAILY,
+  tasks.Recurrence.WEEKLY,
+  tasks.Recurrence.MONTHLY,
+  tasks.Recurrence.QUARTERLY,
+  tasks.Recurrence.BIENNIAL,
+  tasks.Recurrence.YEARLY,
+]);
+const sizes = ref<Sizes[]>([
+  'x-small',
+  'small',
+  'normal',
+  'large',
+  'x-large',
+]);
+const size = ref<Sizes>('normal');
+</script>
+
+<docs lang="md">
+# Recurrence Chip
+
+Show task's reccurence in a styled chip
+</docs>
---
+++ src/vue/src/components/lib/RecurrenceChip.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-chip
+    v-bind="props"
+    color="primary"
+  >
+    {{ $t(value) }}
+  </v-chip>
+</template>
+
+<script lang="ts">
+import type { tasks } from 'ezreeport-sdk-js';
+import { defineComponent, type PropType } from 'vue';
+
+export type Sizes = 'x-small' | 'small' | 'normal' | 'large' | 'x-large';
+
+export default defineComponent({
+  props: {
+    value: {
+      type: String as PropType<tasks.Recurrence>,
+      required: true,
+    },
+    size: {
+      type: String as PropType<Sizes>,
+      default: 'normal',
+    },
+  },
+  computed: {
+    props() {
+      return {
+        [this.size]: true,
+      };
+    },
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
+
+<i18n lang="yaml">
+messages:
+  en:
+    DAILY: Daily
+    WEEKLY: Weekly
+    MONTHLY: Monthly
+    QUARTERLY: Quarterly
+    BIENNIAL: Biennial
+    YEARLY: Yearly
+  fr:
+    DAILY: Quotidien
+    WEEKLY: Hebdomadaire
+    MONTHLY: Mensuel
+    QUARTERLY: Trimestriel
+    BIENNIAL: Bi-annuel
+    YEARLY: Annuel
+</i18n>
