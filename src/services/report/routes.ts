6c11380fcde6925d9a1f8fee2cc343ddfdb96bef
oxypomme <oxytom8@gmail.com>
2023-01-16 09:08:58 +0100

    refactor(report): order routers by alpha-num

--- src/services/report/routes.ts
+++ src/services/report/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import swaggerUi from 'swagger-ui-express';
+import { HTTPError } from './types/errors';
 import openapi from './openapi.json';
 import authRouter from './routes/auth';
 import cronsRouter from './routes/crons';
@@ -12,22 +13,21 @@ import queuesRouter from './routes/queues';
 import tasksRouter from './routes/tasks';
 import templatesRouter from './routes/templates';
 import unsubscribeRouter from './routes/unsubscribe';
-import { HTTPError } from './types/errors';
 
 const router = Router()
   /**
    * Router
    */
-  .use('/templates', templatesRouter)
-  .use('/tasks', tasksRouter)
+  .use('/me', authRouter)
+  .use('/crons', cronsRouter)
+  .use('/reports', filesRouter)
+  .use('/health', healthRouter)
   .use('/history', historyRouter)
   .use('/institutions', institutionsRouter)
-  .use('/reports', filesRouter)
   .use('/queues', queuesRouter)
-  .use('/crons', cronsRouter)
+  .use('/tasks', tasksRouter)
+  .use('/templates', templatesRouter)
   .use('/unsubscribe', unsubscribeRouter)
-  .use('/health', healthRouter)
-  .use('/me', authRouter)
 
   /**
    * API Docs
