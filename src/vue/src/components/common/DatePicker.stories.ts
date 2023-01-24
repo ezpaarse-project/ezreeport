6dd0b45e84c0aecd7d7facc4b04d937e7920a6ca
oxypomme <oxytom8@gmail.com>
2023-01-24 15:07:02 +0100

    feat(vue): added wrapper for v-date-picker

---
+++ src/vue/src/components/common/DatePicker.stories.ts
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/vue';
+import DatePicker from './DatePicker.vue';
+
+const meta: Meta<typeof DatePicker> = {
+  title: 'Utils (Internal)/DatePicker',
+  component: DatePicker,
+  args: {
+    label: 'Story',
+    value: new Date(),
+    min: new Date(2000, 0, 1),
+    max: new Date(3000, 0, 1),
+  },
+  argTypes: {
+    input: { action: 'input' },
+    value: { control: 'date' },
+    min: { control: 'date' },
+    max: { control: 'date' },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof DatePicker>;
+
+export const Basic: Story = {
+  render: (args) => ({
+    components: { DatePicker },
+    props: Object.keys(args),
+    template: '<DatePicker v-bind="$props" v-on="$props" />',
+  }),
+};
