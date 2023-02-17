939a35d37f6b7e4e6320c60babb8a3224f68d395
oxypomme <oxytom8@gmail.com>
2023-02-17 16:13:11 +0100

    feat(vue): added version compatibility into status list

--- src/vue/src/components/public/health/StatusList.vue
+++ src/vue/src/components/public/health/StatusList.vue
@@ -4,6 +4,22 @@
       :text="$t('title').toString()"
       :loading="loading"
     >
+      <v-tooltip top>
+        <template #activator="{ attrs, on }">
+          <v-chip
+            href="https://github.com/ezpaarse-project/ezreeport/releases?q=ezreeport-vue&expanded=true"
+            target="_blank"
+            rel="noopener noreferrer"
+            v-bind="attrs"
+            v-on="on"
+          >
+            v{{ clientVersion }}
+          </v-chip>
+        </template>
+
+        <span>{{$t('version-tooltip.client', { sdk: $ezReeport.sdk.version })}}</span>
+      </v-tooltip>
+
       <RefreshButton
         :loading="loading"
         :tooltip="$t('refresh-tooltip').toString()"
@@ -24,13 +40,33 @@
 
             <v-spacer />
 
+            <v-tooltip top v-if="server.name === status.name">
+              <template #activator="{ attrs, on }">
+                <v-chip
+                  :color="versionCompat.color"
+                  :outlined="!server.version"
+                  href="https://github.com/ezpaarse-project/ezreeport/releases?q=ezreeport-report&expanded=true"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  class="mr-2"
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  v{{ server.version || '...' }}
+                </v-chip>
+              </template>
+
+              <span>{{versionCompat.tooltip}}</span>
+            </v-tooltip>
+
             <v-tooltip
-              top
               :color="status.color"
+              top
             >
               <template #activator="{ on, attrs }">
                 <v-chip
                   :color="status.color"
+                  outlined
                   v-bind="attrs"
                   v-on="on"
                 >
@@ -51,6 +87,7 @@
 <script lang="ts">
 import type { health } from 'ezreeport-sdk-js';
 import { defineComponent } from 'vue';
+import { version } from '~/../package.json';
 
 interface StatusItem {
   name: string,
@@ -61,13 +98,35 @@ interface StatusItem {
 
 export default defineComponent({
   data: () => ({
-    loading: false,
     statuses: [] as health.PingResult[],
+    clientVersion: version,
+    server: {
+      name: '',
+      version: '',
+    },
+
+    loading: false,
     error: '',
   }),
   computed: {
     items(): StatusItem[] {
       return this.statuses.map(this.parsePingResult);
+    },
+    versionCompat() {
+      if (this.server.version) {
+        const [clientMajor, clientMinor] = this.clientVersion.split('.', 3);
+        const [serverMajor, serverMinor] = this.server.version.split('.', 3);
+
+        if (+clientMajor !== +serverMajor) {
+          return { color: 'error', tooltip: this.$t('version-tooltip.mismatch') };
+        }
+
+        if (+clientMinor !== +serverMinor) {
+          return { color: 'warning', tooltip: this.$t('version-tooltip.mismatch') };
+        }
+      }
+
+      return { color: '', tooltip: this.$t('version-tooltip.server') };
     },
   },
   mounted() {
@@ -82,6 +141,13 @@ export default defineComponent({
       try {
         const { content } = await this.$ezReeport.sdk.health.checkAllConnectedService();
         this.statuses = content;
+
+        const { content: result } = await this.$ezReeport.sdk.health.getAllConnectedServices();
+        this.server = {
+          name: result.current,
+          version: result.currentVersion,
+        };
+
         this.error = '';
       } catch (error) {
         this.error = (error as Error).message;
@@ -111,7 +177,15 @@ export default defineComponent({
 en:
   title: 'Status'
   refresh-tooltip: 'Refresh status list'
+  version-tooltip:
+    client: 'Client version (SDK: v{sdk})'
+    server: 'API version'
+    mismatch: 'Client version is not compatible with API version. It may result in unwanted behavior.'
 fr:
   title: 'Status'
   refresh-tooltip: 'Rafraîchir la liste des status'
+  version-tooltip:
+    client: 'Version du Client (SDK: v{sdk})'
+    server: "Version de l'API"
+    mismatch: "La version du client n'est pas compatible avec la version de l'API. Cela peut résulter en un comportement anormal."
 </i18n>
