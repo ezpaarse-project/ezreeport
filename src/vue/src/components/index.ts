09a7dbb1f2dcc2adb8cbacace537d582722bab6a
oxypomme <oxytom8@gmail.com>
2023-03-10 08:03:25 +0100

    fix(vue): fixed example build
    still issues with TS

---
+++ src/vue/src/components/index.ts
@@ -0,0 +1,26 @@
+import StatusList from './public/health/StatusList.vue';
+import CronList from './public/crons/CronList.vue';
+import TasksTable from './public/tasks/TasksTable.vue';
+import HistoryTable from './public/history/HistoryTable.vue';
+import TemplateList from './public/templates/TemplateList.vue';
+import QueueList from './public/queues/QueueList.vue';
+
+export default {
+  'ezr-status-list': StatusList,
+  'ezr-cron-list': CronList,
+  'ezr-task-table': TasksTable,
+  'ezr-history-table': HistoryTable,
+  'ezr-template-list': TemplateList,
+  'ezr-queue-list': QueueList,
+};
+
+declare module 'vue' {
+  export interface GlobalComponents {
+    EzrStatusList: typeof StatusList,
+    EzrCronList: typeof CronList,
+    EzrTasksTable: typeof TasksTable,
+    EzrHistoryTable: typeof HistoryTable,
+    EzrTemplateList: typeof TemplateList,
+    EzrQueueList: typeof QueueList,
+  }
+}
