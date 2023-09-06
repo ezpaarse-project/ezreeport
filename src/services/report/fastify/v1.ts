8bc49324ca15a65d5cbfcd49335b01fa72d06c48
oxypomme <oxytom8@gmail.com>
2023-09-06 11:39:31 +0200

    refactor(report): moved doc files

--- src/services/report/fastify/v1.ts
+++ src/services/report/fastify/v1.ts
@@ -9,22 +9,17 @@ import health from './v1/health';
 const router: FastifyPluginAsync = async (fastify) => {
   fastify.decorateReply('apiVersion', 1);
 
-  // Register swagger-ui dist files as static (on /doc)
+  // Register doc files as static (on /doc)
   await fastify.register(
     fastifyStatic,
     {
       prefix: '/doc/',
-      root: swaggerUiPath(),
+      root: [
+        join(__dirname, 'v1/doc'), // swagger-ui overrides + open api
+        swaggerUiPath(), // swagger-ui
+      ],
     },
   );
-  // Overriding default Swagger-UI initializer with custom one
-  fastify.get('/doc/swagger-initializer.js', (request, reply) => {
-    reply.sendFile('openapi.js', join(__dirname, 'v1'));
-  });
-  // Serving OpenAPI
-  fastify.get('/doc/openapi.json', (request, reply) => {
-    reply.sendFile('openapi.json', join(__dirname, 'v1'));
-  });
 
   await fastify.register(auth, { prefix: '/me' });
   await fastify.register(health, { prefix: '/health' });
