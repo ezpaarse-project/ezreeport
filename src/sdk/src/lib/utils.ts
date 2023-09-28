cb8d7d32ad4b11be57ed64a2633b49230555eeb6
oxypomme <oxytom8@gmail.com>
2023-09-28 16:49:58 +0200

    fix(sdk): fixed types of fetch option

--- src/sdk/src/lib/utils.ts
+++ src/sdk/src/lib/utils.ts
@@ -1,10 +1,5 @@
 import { parseISO } from 'date-fns';
 
-// Extracted from Prisma
-export type JsonObject = { [Key in string]?: JsonValue };
-export type JsonArray = JsonValue[];
-export type JsonValue = string | number | boolean | JsonObject | JsonArray | null;
-
 /**
  * Async version of `setTimeout`
  *
