fa3a0c6241b30ee1a01bce616bf1a73624e8acc7
oxypomme <oxytom8@gmail.com>
2023-10-02 10:31:50 +0200

    fix(mail): fixed mail sending

--- src/services/mail/lib/bull/jobs/sendReportMail.ts
+++ src/services/mail/lib/bull/jobs/sendReportMail.ts
@@ -16,6 +16,161 @@ const {
   api: { url: APIurl },
 } = config;
 
+type ErrorReportPrams = {
+  data: MailResult;
+  options: Omit<MailOptions, 'to' | 'body' | 'subject'>;
+  bodyData: {
+    recurrence: string;
+    name: string;
+    date:string;
+  };
+  filename: string;
+  date: Date;
+  dateStr: string;
+};
+
+const sendErrorReport = async ({
+  data,
+  options,
+  bodyData,
+  filename,
+  date,
+  dateStr,
+}: ErrorReportPrams) => {
+  // TODO[feat]: Ignore team if test report ?
+  const to = [data.contact ?? '', team];
+
+  let error: string;
+  try {
+    const { detail } = JSON.parse(b64ToString(data.file));
+    error = detail.error.message;
+  } catch (err) {
+    error = 'Unknown error, see attachements';
+  }
+
+  await sendMail({
+    ...options,
+    to,
+    subject: `Erreur de Reporting ezMESURE [${dateStr}] - ${data.task.name}`,
+    body: await generateMail('error', { ...bodyData, error, date: format(date, 'dd/MM/yyyy à HH:mm:ss') }),
+  });
+  logger.info(`[mail] [${process.pid}] Error report [${filename}] sent to [${to.filter((v) => v).join(', ')}]`);
+};
+
+type SuccessReportParams = {
+  filename: string;
+  dateStr: string;
+  options: Omit<MailOptions, 'to' | 'body' | 'subject'>;
+  bodyData: {
+    recurrence: string;
+    name: string;
+    date: string;
+  };
+  task: MailResult['task'];
+};
+
+const sendSuccessReport = async ({
+  task,
+  options,
+  dateStr,
+  bodyData,
+  filename,
+}: SuccessReportParams) => {
+  // Send one email per target to allow un-subscription prefill
+  const targets = await Promise.allSettled(
+    task.targets.map(async (to) => {
+      try {
+        const taskId64 = stringToB64(task.id);
+        const to64 = stringToB64(to);
+        const unsubId = encodeURIComponent(`${taskId64}:${to64}`);
+
+        const unsubscribeLink = `${APIurl}/unsubscribe/${unsubId}`;
+        await sendMail({
+          ...options,
+          to,
+          subject: `Reporting ezMESURE [${dateStr}] - ${task.name}`,
+          body: await generateMail('success', { ...bodyData, unsubscribeLink }),
+        });
+
+        return to;
+      } catch (error) {
+        if (error instanceof Error) {
+          logger.error(`[mail] [${process.pid}] Report [${filename}] wan't sent to [${to}] with error: {${error.message}}`);
+        } else {
+          logger.error(`[cron] [${process.pid}] Unexpected error when sending report [${filename}] wan't sent to [${to}]: {${error}}`);
+        }
+        throw error;
+      }
+    }),
+  );
+
+  const successTargets = targets.filter(isFulfilled).map(({ value }) => value);
+  if (successTargets.length > 0) {
+    logger.info(`[mail] [${process.pid}] Report [${filename}] sent to [${successTargets.join(', ')}]`);
+  } else {
+    logger.error(`[mail] [${process.pid}] Report [${filename}] wasn't sent to anyone (see previous logs)`);
+  }
+};
+
+const sendReport = (data: MailResult, date: Date, dateStr: string) => {
+  const filename = data.url.replace(/^.*\//, '');
+
+  const options: Omit<MailOptions, 'to' | 'body' | 'subject'> = {
+    attachments: [{
+      filename,
+      content: data.file,
+      encoding: 'base64',
+    }],
+  };
+  const bodyData = {
+    recurrence: recurrenceToStr(data.task.recurrence),
+    name: data.task.name,
+    date: dateStr,
+  };
+
+  if (data.success) {
+    return sendSuccessReport({
+      filename,
+      dateStr,
+      options,
+      bodyData,
+      task: data.task,
+    });
+  }
+  return sendErrorReport({
+    data,
+    bodyData,
+    date,
+    dateStr,
+    filename,
+    options,
+  });
+};
+
+type ErrorParams = {
+  file: string,
+  filename: string,
+  contact: string,
+  date: string,
+};
+
+const sendError = async (data: ErrorParams, date: Date, dateStr: string) => {
+  await sendMail({
+    attachments: [{
+      filename: data.filename,
+      content: data.file,
+      encoding: 'base64',
+    }],
+    to: [team],
+    subject: `Erreur de Reporting ezMESURE [${dateStr}]`,
+    body: await generateMail('error', {
+      error: 'Unknown error, see attachements',
+      date: format(date, 'dd/MM/yyyy à HH:mm:ss'),
+    }),
+  });
+  logger.info(`[mail] [${process.pid}] Error sent to [${team}]`);
+};
+
 export default async (j: Job) => {
   // Re-getting job from it's id and queue to get child jobs
   // See https://github.com/taskforcesh/bullmq/issues/753
@@ -25,78 +180,26 @@ export default async (j: Job) => {
     throw new Error(`Cannot find job [${j.id}] in queue [${queue.name}]`);
   }
 
-  const data: MailResult = Object.values(await job.getChildrenValues())[0].mailData;
+  const data: MailResult = Object.values(await job.getChildrenValues())[0]?.mailData;
 
-  const filename = data.url.replace(/^.*\//, '');
   const date = parseISO(data.date);
   const dateStr = format(date, 'dd/MM/yyyy');
 
   try {
-    const options: Omit<MailOptions, 'to' | 'body' | 'subject'> = {
-      attachments: [{
-        filename,
-        content: data.file,
-        encoding: 'base64',
-      }],
-    };
-    const bodyData = {
-      recurrence: recurrenceToStr(data.task.recurrence),
-      name: data.task.name,
-      date: dateStr,
-    };
-
-    if (data.success) {
-      // Send one email per target to allow un-subscription prefill
-      const targets = await Promise.allSettled(
-        data.task.targets.map(async (to) => {
-          try {
-            const taskId64 = stringToB64(data.task.id);
-            const to64 = stringToB64(to);
-            const unsubId = encodeURIComponent(`${taskId64}:${to64}`);
-
-            const unsubscribeLink = `${APIurl}/unsubscribe/${unsubId}`;
-            await sendMail({
-              ...options,
-              to,
-              subject: `Reporting ezMESURE [${dateStr}] - ${data.task.name}`,
-              body: await generateMail('success', { ...bodyData, unsubscribeLink }),
-            });
-
-            return to;
-          } catch (error) {
-            logger.error(`[mail] Report "${filename}" wan't sent to ${to} with error: ${(error as Error).message}`);
-            throw error;
-          }
-        }),
-      );
-
-      const successTargets = targets.filter(isFulfilled).map(({ value }) => value);
-      if (successTargets.length > 0) {
-        logger.info(`[mail] Report "${filename}" sent to [${successTargets.join(', ')}]`);
-      } else {
-        logger.error(`[mail] Report "${filename}" wasn't sent to anyone (see previous logs)`);
-      }
-    } else {
-      // TODO[feat]: Ignore team if test report ?
-      const to = [data.contact ?? '', team];
-
-      let error: string;
-      try {
-        const { detail } = JSON.parse(b64ToString(data.file));
-        error = detail.error.message;
-      } catch (err) {
-        error = 'Unknown error, see attachements';
-      }
-
-      await sendMail({
-        ...options,
-        to,
-        subject: `Erreur de Reporting ezMESURE [${dateStr}] - ${data.task.name}`,
-        body: await generateMail('error', { ...bodyData, error, date: format(date, 'dd/MM/yyyy à HH:mm:ss') }),
-      });
-      logger.info(`[mail] Error report "${filename}" sent to [${to.filter((v) => v).join(', ')}]`);
+    if (data && !job.data.error) {
+      await sendReport(data, date, dateStr);
     }
+
+    if (job.data.error) {
+      await sendError(job.data, date, dateStr);
+    }
+
+    throw new Error('No suitable data found');
   } catch (error) {
-    logger.error(`[mail] Error when sending Report "${filename}" : ${(error as Error).message}`);
+    if (error instanceof Error) {
+      logger.error(`[mail] [${process.pid}] Error when sending incoming report: {${error.message}}`);
+    } else {
+      logger.error(`[cron] [${process.pid}] Unexpected error when sending incoming report: {${error}}`);
+    }
   }
 };
