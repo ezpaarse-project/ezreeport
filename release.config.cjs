993130167b0bc8f5a54b9f528b8f2f8bcabee95c
oxypomme <oxytom8@gmail.com>
2024-04-09 14:57:44 +0200

    build: changed release method

---
+++ release.config.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  $schema: 'https://raw.githubusercontent.com/SchemaStore/schemastore/master/src/schemas/json/semantic-release.json',
+  extends: [
+    'semantic-release-config',
+  ],
+
+  ci: false,
+  tagFormat: `${process.env.PNPM_PACKAGE_NAME}@\${version}`,
+  plugins: [
+    ['@semantic-release/git', { assets: ['CHANGELOG.md'] }],
+  ],
+  branches: [
+    'master',
+    {
+      name: 'rc/*',
+      prerelease: 'rc',
+    },
+    {
+      name: 'dev',
+      prerelease: 'beta',
+    },
+    {
+      name: 'build/*',
+      prerelease: 'alpha',
+    },
+  ],
+};
