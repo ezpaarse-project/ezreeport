64247b46c4f11ccac9ff5b586847f2457c8c0d6f
oxypomme <oxytom8@gmail.com>
2024-08-28 14:56:12 +0200

    feat(ezra): added migration script for templates v2 (2.0.0)

--- services/ezra/src/migrations/index.ts
+++ services/ezra/src/migrations/index.ts
@@ -1,9 +1,11 @@
 // import migration from './version.js';
 import migrate100Beta19 from './1_0_0-beta_19.js';
 import migrate100Beta22 from './1_0_0-beta_22.js';
+import migrate200 from './2_0_0.js';
 
 export default {
   // [migration.name]: migration
   [migrate100Beta19.name]: migrate100Beta19,
   [migrate100Beta22.name]: migrate100Beta22,
+  [migrate200.name]: migrate200,
 };
