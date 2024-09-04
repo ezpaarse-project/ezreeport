5986bdd8680f5537868fd8e8a506f27c1298ba60
oxypomme <oxytom8@gmail.com>
2024-09-04 16:59:05 +0200

    refactor: using new transports from pino

--- services/report/src/lib/logger.ts
+++ services/report/src/lib/logger.ts
@@ -1,52 +1,51 @@
 import pino from 'pino';
-import pretty from 'pino-pretty';
 
 import { resolve } from 'node:path';
 
 import config from '~/lib/config';
 
-type LoggerOptions = Omit<pino.LoggerOptions, 'level'> & { name: string };
+type LoggerOptions = Omit<pino.LoggerOptions, 'level' | 'transports'> & { name: string };
 
 const { level: l, dir, ignore } = config.log;
 const level = l as pino.Level;
 
-function getStdOut() {
+function getStdOutTarget(): pino.TransportTargetOptions {
   if (process.env.NODE_ENV === 'production') {
-    return process.stdout;
+    return { target: 'pino/file', options: { destination: 1 } };
   }
 
-  // Setup pretty logger on dev
-  return pretty({
-    ignore: [...ignore, 'scope'].join(','),
-    colorize: true,
-    messageFormat: ({ scope, msg }) => {
-      if (scope) {
-        return `[${scope}] ${msg || ''}`;
-      }
-      return `${msg || ''}`;
+  return {
+    target: 'pino-pretty',
+    options: {
+      ignore: [...ignore, 'scope'].join(','),
+      colorize: true,
+      messageFormat: '{if scope}[{scope}]{end} {msg}',
     },
-  });
+  };
 }
 
 function createLogger(options: LoggerOptions) {
-  const streams: pino.StreamEntry[] = [{ level, stream: getStdOut() }];
+  const targets: pino.TransportTargetOptions[] = [
+    { level, ...getStdOutTarget() },
+  ];
 
   // If needed add logs into a file
   if (dir) {
-    streams.push({
-      level,
-      stream: pino.destination({
-        dest: resolve(dir, `${options.name}.log`),
+    targets.push({
+      target: 'pino/file',
+      options: {
+        destination: resolve(dir, `${options.name}.log`),
         sync: false,
         ignore: ignore.join(','),
-      }),
+      },
     });
   }
 
-  return pino(
-    { ...options, level },
-    pino.multistream(streams),
-  );
+  return pino({
+    ...options,
+    level,
+    transport: { targets },
+  });
 }
 
 export const accessLogger = createLogger({ name: 'access' });
