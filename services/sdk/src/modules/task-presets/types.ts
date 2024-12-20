ff90044efe2ee5b1371612a88c07f224b2c599be
oxypomme <oxytom8@gmail.com>
2024-12-20 16:25:50 +0100

    feat(sdk): updated task-presets module for API v2
    BREAKING CHANGE: parameters and return types changed

---
+++ services/sdk/src/modules/task-presets/types.ts
@@ -0,0 +1,57 @@
+import type { TemplateFilter, TemplateTag } from '~/modules/templates/types';
+import type { TaskRecurrence } from '~/modules/tasks/types';
+
+export interface TaskPreset {
+  /** Preset ID */
+  id: string;
+  /** Preset name */
+  name: string;
+  /** Template ID */
+  templateId: string;
+  /** Recurrence */
+  recurrence: TaskRecurrence;
+  /** Options used to fetch data for the report */
+  fetchOptions: {
+    /** Default elastic date field to fetch data from */
+    dateField: string;
+    /** Default elastic index to fetch data from */
+    index: string;
+  };
+  /** If preset is hidden */
+  hidden: boolean;
+  /** Preset creation date */
+  createdAt: Date;
+  /** Preset last update date */
+  updatedAt?: Date;
+
+  /** Template referenced by the preset, must be included when fetching */
+  template?: {
+    /** Template tags */
+    tags?: TemplateTag[];
+    /** If template is hidden */
+    hidden?: boolean;
+  };
+}
+
+/**
+ * Task preset in raw format
+ */
+export interface RawTaskPreset extends Omit<TaskPreset, 'createdAt' | 'updatedAt'> {
+  createdAt: string;
+  updatedAt?: string | null;
+}
+
+export type InputTaskPreset = Omit<TaskPreset, 'id' | 'createdAt' | 'updatedAt' | 'template'>;
+
+export interface AdditionalDataForPreset {
+  /** Task name */
+  name: string;
+  /** Namespace ID of the task */
+  namespaceId: string;
+  /** Elastic index to fetch data from */
+  index: string;
+  /** Email addresses to send report */
+  targets: string[];
+  /** Global filters used when fetching data */
+  filters?: TemplateFilter[];
+}
