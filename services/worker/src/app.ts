73d71c0383e3ddd9602f532c57df87a9724134eb
oxypomme <oxytom8@gmail.com>
2025-03-11 15:16:25 +0100

    fix(worker): added log line when starting

--- services/worker/src/app.ts
+++ services/worker/src/app.ts
@@ -1,10 +1,19 @@
 import { appLogger } from '~/lib/logger';
+import config from '~/lib/config';
 
 import initQueue from '~/models/queues';
 import { initRenderEngine } from '~/models/render';
 import { initHttp } from './lib/http-requests';
 
 const start = async () => {
+  appLogger.info({
+    scope: 'node',
+    env: process.env.NODE_ENV,
+    logLevel: config.log.level,
+    logDir: config.log.dir,
+    msg: 'Service starting',
+  });
+
   await initQueue();
 
   await initRenderEngine();
