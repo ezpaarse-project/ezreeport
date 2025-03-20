f62ddb859849a9e551e1cd17f7a786bc6fa15d59
oxypomme <oxytom8@gmail.com>
2025-03-20 22:47:03 +0100

    fix(vue): fixed issue with dates in vuetify components

--- services/vue/src/plugins/vuetify.ts
+++ services/vue/src/plugins/vuetify.ts
@@ -1,8 +1,12 @@
 import 'vuetify/styles';
 import { createVuetify, type VuetifyOptions } from 'vuetify';
 import { aliases, mdi } from 'vuetify/iconsets/mdi';
+import DateFnsAdapter from '@date-io/date-fns';
 
 export const config: VuetifyOptions = {
+  date: {
+    adapter: DateFnsAdapter,
+  },
   icons: {
     defaultSet: 'mdi',
     aliases,
