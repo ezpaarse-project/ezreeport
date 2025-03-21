46e949cd184499897b7384d8c02261f03fe114af
oxypomme <oxytom8@gmail.com>
2025-03-21 22:51:23 +0100

    feat(scheduler): added dead lettering for generations
    generations have now a distinct createdAt and startedAt

--- services/scheduler/src/models/queues/report/generation.ts
+++ services/scheduler/src/models/queues/report/generation.ts
@@ -3,24 +3,94 @@ import { randomUUID } from 'node:crypto';
 import type rabbitmq from '~/lib/rabbitmq';
 import { appLogger } from '~/lib/logger';
 
-import type { GenerationQueueDataType } from '~common/types/queues';
+import { GenerationQueueData, type GenerationQueueDataType } from '~common/types/queues';
 import type { GenerationType } from '~common/types/generations';
 
 const generationQueueName = 'ezreeport.report:queues';
+const deadGenerationExchangeName = 'ezreeport.report:queues:dead';
 
 const logger = appLogger.child({ scope: 'queues', queue: generationQueueName });
 
 let channel: rabbitmq.Channel | undefined;
 
-export function initGenerationQueue(c: rabbitmq.Channel) {
+async function onDeadGeneration(c: rabbitmq.Channel, msg: rabbitmq.ConsumeMessage | null) {
+  if (!msg) {
+    return;
+  }
+
+  // Parse message
+  const raw = JSON.parse(msg.content.toString());
+  let data;
+  try {
+    data = await GenerationQueueData.parseAsync(raw);
+  } catch (err) {
+    logger.error({
+      msg: 'Invalid data',
+      data: process.env.NODE_ENV === 'production' ? undefined : raw,
+      err,
+    });
+    c.nack(msg, undefined, false);
+    return;
+  }
+
+  try {
+    const event: GenerationType = {
+      id: data.id,
+      taskId: data.task.id,
+      status: 'ABORTED',
+      start: data.period.start,
+      end: data.period.end,
+      targets: data.targets,
+      origin: data.origin,
+      writeActivity: !!data.writeActivity,
+      progress: null,
+      took: null,
+      reportId: '',
+      createdAt: data.createdAt,
+      updatedAt: new Date(),
+      startedAt: null,
+    };
+
+    const buf = Buffer.from(JSON.stringify(event));
+    c.publish('ezreeport.report:event', '', buf);
+  } catch (err) {
+    logger.error({ msg: 'Failed to send event', err });
+  }
+  c.ack(msg);
+}
+
+export async function initGenerationQueue(c: rabbitmq.Channel) {
   // queueGeneration will be called while begin unaware of
   // rabbitmq connection, so we need to store the channel
   // here
   channel = c;
+
+  const { exchange: deadLetterExchange } = await c.assertExchange(
+    deadGenerationExchangeName,
+    'fanout',
+    { durable: false },
+  );
+
+  const { queue: deadLetterQueue } = await c.assertQueue(
+    '',
+    { exclusive: true, durable: false, deadLetterExchange },
+  );
+  channel.consume(deadLetterQueue, (m) => onDeadGeneration(c, m));
+
+  await c.bindQueue(deadLetterQueue, deadLetterExchange, '');
+
+  // Ensure generation queue exists with correct dead letter exchange
+  await c.assertQueue(
+    generationQueueName,
+    { durable: false, deadLetterExchange },
+  );
+
+  logger.debug('Generation queue created');
 }
 
 // eslint-disable-next-line import/prefer-default-export
-export async function queueGeneration(params: Omit<GenerationQueueDataType, 'id'>) {
+export async function queueGeneration(params: Omit<GenerationQueueDataType, 'id' | 'createdAt'>) {
+  const createdAt = new Date();
   let data: GenerationQueueDataType;
   try {
     if (!channel) {
@@ -30,6 +100,7 @@ export async function queueGeneration(params: Omit<GenerationQueueDataType, 'id'
     data = {
       ...params,
       id: randomUUID(),
+      createdAt,
     };
 
     const buf = Buffer.from(JSON.stringify(data));
@@ -57,9 +128,13 @@ export async function queueGeneration(params: Omit<GenerationQueueDataType, 'id'
       end: data.period.end,
       targets: data.targets,
       origin: data.origin,
+      writeActivity: !!data.writeActivity,
+      progress: null,
+      took: null,
       reportId: '',
-      createdAt: new Date(),
+      createdAt,
       updatedAt: new Date(),
+      startedAt: null,
     };
 
     const buf = Buffer.from(JSON.stringify(event));
