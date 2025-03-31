c44a4d65c2412b025408be47b7099444bb05f782
oxypomme <oxytom8@gmail.com>
2025-03-31 11:44:04 +0200

    fix(vue): disabling animation when dragging

--- services/vue/src/components/private/editor/preview/Layout.vue
+++ services/vue/src/components/private/editor/preview/Layout.vue
@@ -8,7 +8,7 @@
       rounded
       class="template-layout-preview"
     >
-      <v-fade-transition tag="div" group class="template-layout-preview-grid">
+      <v-fade-transition tag="div" group class="template-layout-preview-grid" :disabled="dragging">
         <EditorPreviewSlot
           v-for="figure in modelValue.figures"
           :key="figure.id"
@@ -31,6 +31,7 @@
 </template>
 
 <script setup lang="ts">
+import { state } from '@formkit/drag-and-drop';
 import type { AnyLayoutHelper } from '~sdk/helpers/layouts';
 
 // Components props
@@ -42,6 +43,16 @@ defineProps<{
 }>();
 
 const { grid } = useTemplateEditor();
+
+const dragging = ref(false);
+
+state.on('dragStarted', () => {
+  dragging.value = true;
+});
+
+state.on('dragEnded', () => {
+  dragging.value = false;
+});
 </script>
 
 <style lang="scss" scoped>
