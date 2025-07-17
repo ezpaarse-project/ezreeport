97f562336baf85311f01f27a75f68032b7581b72
oxypomme <oxytom8@gmail.com>
2025-07-17 13:49:12 +0200

    chore: fixed publish script

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,3 +1,3 @@
 HUSKY=0 \
-  pnpm -r --workspace-concurrency=1 --filter ./services \
+  pnpm -r --workspace-concurrency=1 --filter "./services/*" --filter "./packages/database" \
   exec -- npx --no-install semantic-release
