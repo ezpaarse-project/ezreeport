7f284675abc3b241ce32a140550af4b44b907e74
oxypomme <oxytom8@gmail.com>
2025-07-22 08:21:02 +0200

    chore(vue): fixed locales in storybook

--- services/vue/.storybook/i18n.ts
+++ services/vue/.storybook/i18n.ts
@@ -1,14 +1,15 @@
 import { createI18n } from 'vue-i18n';
 import { en as enV, fr as frV } from 'vuetify/locale';
 
-import { en as enR, fr as frR } from '../src/locale';
+import enLocale from '../src/locales/en.json';
+import frLocale from '../src/locales/fr.json';
 
 // oxlint-disable-next-line no-explicit-any
 export default createI18n<any>({
   locale: 'en',
   fallbackLocale: 'en',
   messages: {
-    en: { $vuetify: enV, $ezreeport: enR },
-    fr: { $vuetify: frV, $ezreeport: frR },
+    en: { $vuetify: enV, ...enLocale },
+    fr: { $vuetify: frV, ...frLocale },
   },
 });
