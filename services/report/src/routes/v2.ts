c7b4916ff839b656860c6b1c7198f4b65d3427ab
oxypomme <oxytom8@gmail.com>
2025-10-17 14:53:30 +0200

    fix(api): fixed zod errors format

--- services/report/src/routes/v2.ts
+++ services/report/src/routes/v2.ts
@@ -12,8 +12,6 @@ import {
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod';
 
-import { simplifyZodIssue } from '@ezreeport/models/lib/zod';
-
 import openapi from '~/plugins/openapi';
 
 import { NotFoundError } from '~/models/errors';
@@ -35,27 +33,35 @@ const router: FastifyPluginAsync = async (fastify) => {
     let status = StatusCodes.INTERNAL_SERVER_ERROR;
     let error: Error | undefined;
 
+    // If it's a http error
+    if (err.statusCode) {
+      status = err.statusCode;
+      error = err;
+    }
+
     // If it's a request validation error
     if (hasZodFastifySchemaValidationErrors(err)) {
       status = StatusCodes.BAD_REQUEST;
       error = new Error("Request doesn't match the schema", {
-        cause: err.validation,
+        cause: {
+          issues: err.validation,
+          context: err.validationContext,
+        },
       });
     }
 
     // If it's a response validation error
     if (isResponseSerializationError(err)) {
-      const cause = err.cause.issues.map((issue) => simplifyZodIssue(issue));
+      status = StatusCodes.INTERNAL_SERVER_ERROR;
       error = new Error(
         "Response doesn't match the schema. Please contact the administrators",
-        { cause }
+        {
+          cause: {
+            issues: err.cause.issues,
+            context: 'response',
+          },
+        }
       );
-    }
-
-    // If it's a http error
-    if (err.statusCode) {
-      status = err.statusCode;
-      error = err;
     }
 
     return reply.status(status).send(buildErrorResponse(error ?? err, reply));
